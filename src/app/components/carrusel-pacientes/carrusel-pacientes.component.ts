6b941177d306badf79782856b7975bf2bb16fc18
CarlaZumaran <carla.z@rompecabeza.cl>
2018-08-30 11:40:31 -0300

    Cambio iconos asesor y paciente

--- src/app/components/carrusel-pacientes/carrusel-pacientes.component.ts
+++ src/app/components/carrusel-pacientes/carrusel-pacientes.component.ts
@@ -9,6 +9,21 @@ export class CarruselPacientesComponent implements OnInit {
 
   constructor() { }
 
+  patient: any[] = [
+    {
+      logo: 'assets/icons/icon-woman.svg',
+      // logo-hombre: 'assets/icons/icon_hombre.svg',
+      // logo-sinGenero: 'assets/icons/icon_singenero.svg',
+      name: 'Francisca Andrea Velasco Ferrada',
+      genre: 'M',
+      age: '30 años, 2 meses y 8 días',
+      mail: 'frnacisca.v@gmail.com',
+      cel: '+569 6222 4567',
+      rut: '16.456.789-9',
+      prev: 'Isapre CruzBlanca'
+    }
+  ];
+
   ngOnInit() {
   }
 
