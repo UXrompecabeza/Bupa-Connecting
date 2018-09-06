5a0bd801cdd4738814f2a9bcb02d5c7f858fd412
CarlaZumaran <carla.z@rompecabeza.cl>
2018-09-06 19:04:50 -0300

    Nuevo select hora tabla examenes

--- src/app/components/table-exams/table-exams.component.ts
+++ src/app/components/table-exams/table-exams.component.ts
@@ -13,30 +13,40 @@ export class TableExamsComponent implements OnInit {
   collection: any[] = [
     {
       info: {
+        index: 0,
         center: ['Integramédica Manquehue','Av. Manquehue 123, Las Condes'],
-        date: ['10:15 am','05/05/2018'],
+        date: ['05/05/2018'],
         price: '48.500',
+        hours: ['10:00','10:30','10:45','11:15'],
+        selectedHour: '10:00',
         doctor: 'Eduardo Navarro Alarcón',
         image: './assets/icons/icon-cv-doctor.svg'
       }
     }, {
       info: {
+        index: 1,
         center: ['Integramédica Las Condes','Las Condes noroeste segundo 123, Las Condes'],
-        date: ['11:15 am','05/05/2018'],
+        date: ['05/05/2018'],
         price: '58.500',
+        hours: ['12:00','12:30','12:45','13:15'],
+        selectedHour: '12:00',
         doctor: 'María Nuñez Muñoz',
         image: './assets/icons/icon-cv-doctor.svg'
       }
     }, {
       info: {
+        index: 2,
         center: ['Clínica Bupa Santiago','Av La Florida 123, La Florida'],
-        date: ['9:15 am','05/05/2018'],
+        date: ['05/05/2018'],
+        hours: ['15:00','15:30','15:45','17:15'],
+        selectedHour: '15:00',
         price: '40.500',
         doctor: 'Pedro Hidalgo Pino',
         image: './assets/icons/icon-cv-doctor.svg'
       }
     }, 
   ];
+
   sortedCollection: any[];
   
   constructor(private orderPipe: OrderPipe) {
